8211ad604b6ae2af2fd4107bb640ed61ef9f5afd
lizijian <camskywalker@outlook.com>
2020-01-05 20:31:10 +0800

    Initial commit

---
+++ src/test/java/com/lizijian/springcloudeurekaserver/SpingcloudlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizijian.springcloudeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingcloudlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
